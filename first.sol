3f3f3c30edfb0b3dca2e0085b55d66d45612a6a8
Sharif35-958 <ssharif222.ss@gmail.com>
2026-02-14 14:49:41 +0600

    getNum

--- first.sol
+++ first.sol
@@ -3,5 +3,12 @@ pragma solidity ^0.8.13;
 contract SimpleStorage {
    string public welcome= "Hello Everyone";
    uint256 public num=10;
+   bool isActive = false;
+   address public MyAdress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+   function getnum() public view returns(uint256)  {
+return num;
+
+   }
    
 }   
